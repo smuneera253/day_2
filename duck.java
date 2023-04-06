21956a8ca2f468282e1787822eb93de017697c5c
smuneera253 <119492034+smuneera253@users.noreply.github.com>
2023-04-07 02:46:28 +0530

    done
    done

---
+++ duck.java
@@ -0,0 +1,12 @@
+//import java.util.Scanner;
+class Kiran {
+   public static void main(String[] args){
+   	//Scanner input = new Scanner(System.in);
+   	String num = "513";
+   	if(num.charAt(0) == '0'){
+   		System.out.println("Not Duck Number");
+   	}else {
+   		System.out.println("Duck Number");
+   	}
+   }
+}
